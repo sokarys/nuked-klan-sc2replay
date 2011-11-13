11d7b766dced926ecd6d7932f0d44fe213a19f32
olivier.locci <olivier.locci@340db772-5fbf-92bd-57be-68c06aa05664>
2011-11-13 15:19:06 +0000

    Style : bold, underline, italique pris en compte

---
+++ trunk/sc2ReplayNk/createSQL.sql
@@ -0,0 +1,31 @@
+CREATE TABLE IF NOT EXISTS `_replay` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `recordby` varchar(64) NOT NULL,
+  `mapname` varchar(64) NOT NULL,
+  `gamelength` varchar(64) NOT NULL,
+  `date` varchar(64) NOT NULL,
+  `gamespeed` varchar(64) NOT NULL,
+  `replayname` varchar(64) NOT NULL,
+  `teamsize` varchar(64) NOT NULL,
+  `commentaire` text NOT NULL,
+  `dateupload` varchar(64) NOT NULL DEFAULT '0',
+  `version` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `replayname` (`replayname`)
+);
+
+
+CREATE TABLE IF NOT EXISTS `_replayPlayer` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `idreplay` int(11) NOT NULL,
+  `nomplayer` varchar(64) NOT NULL,
+  `team` int(11) NOT NULL,
+  `apm` int(11) NOT NULL,
+  `race` varchar(64) NOT NULL,
+  `color` varchar(64) NOT NULL,
+  `winner` varchar(64) NOT NULL,
+  `apmpicture` varchar(128) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY `idreplay` REFERENCES _replay(`id`) ON DELETE CASCADE
+);
+
